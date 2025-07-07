82662d9959b6fad07c91d321b1fcdedf15ee81f4
sbotero120 <santiagobotero096@gmail.com>
2025-07-06 21:51:17 -0500

    Primitive data type

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,19 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        byte age = 18;
+        int largeNumber = 1000;
+        float decimal = 18.5f;
+        double decimalTwo = 20.4;
+
+        boolean isActive = false;
+        char firstLetter = 'S';
+
+        System.out.println("Your age is: " + age);
+        System.out.println("Your big number is: " + largeNumber);
+        System.out.println("Your number with decimals is: " + decimal);
+        System.out.println("Your other decimal is: " + decimalTwo);
+        System.out.println("Your statement is: " + isActive);
+        System.out.println("Your first letter of your name is: " + firstLetter);
     }
 }
