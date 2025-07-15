c7d2ee0f4980700ebcbc8dc5c4717ceb90ff2357
sbotero120 <santiagobotero096@gmail.com>
2025-07-14 23:15:31 -0500

    was added: Escape characters

---
+++ Escape characters/escapeCharacters/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String quotes = "this does not have quotes,\"this does have quotes\"";
+        System.out.println(quotes);
+
+        System.out.println("this is how you set paths:" + " C:\\User \"file\">:");
+        System.out.println("this is how you write text separated by lines: " + "\nhello \nwhat are you doing?");
+        System.out.println("this is how you set the tab character " + "hello_(\t)_what are you doing?");
+    }
+}
